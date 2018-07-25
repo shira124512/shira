3b2c916c9371ef73b82d00c29238c8d4e420ad76
shira124512 <shira124512@gmail.com>
2018-07-25 15:02:18 +0300

    veu

---
+++ VueJS_42584_2-master/Day 02 - 24.07.2018/day 03-project/day 03 project.vue
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+</head>
+<body>
+  
+
+   <script>
+            let allUsers = localStorage.getItem("all task");
+            allUsers = (allUsers) ? JSON.parse(allUsers) : [];
+            let userList = "";
+
+            for(let singleUser of allUsers) {
+                
+                userList += `
+                    <li><h2>date: </h2> ${singleUser.dateO}</li>
+                    <li><h2>time: </h2> ${singleUser.timeO}</li>
+                    <li><h2>task: </h2> ${singleUser.taskO}</li>`;
+
+            }
+            document.getElementById("userList1").innerHTML = userList;
+       
+    
+   </script>
+
+</body>
+</html>
